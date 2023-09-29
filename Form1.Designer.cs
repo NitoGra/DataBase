a6c34dac12eff2c838682eade4768e52e9844b92
NitoGra <131981505+NitoGra@users.noreply.github.com>
2023-09-29 13:09:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace DataBaseButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(459, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(459, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.textBox1, "Не длиннее пяти символов");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(459, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.textBox2, "Не более 999 уровня и не менее 1");
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Добавить нового игрока";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Уровень";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(459, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.textBox3, "Использовать только цифры");
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ID";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(459, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 28);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Бан";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button3.Location = new System.Drawing.Point(500, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 28);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "РазБан";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button4.Location = new System.Drawing.Point(567, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 28);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AutoSize = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(94, 13);
+            this.listBox1.TabIndex = 16;
+            this.listBox1.Text = "тут вывод текста";
+            this.listBox1.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Подсказка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 198);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DataBaseButton";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label listBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DataBaseButton
+{
+    public partial class Form1 : Form
+    {
+        static bool rCode = false;
+        static bool eCode = false;
+        static bool dCode = false;
+        static readonly int maxLengthName = 10;
+        static readonly int maxLengthLevel = 999;
+
+        class DataBase
+        {
+            private Dictionary<int, Player> _players = new Dictionary<int, Player>();
+            private List<string> _names = new List<string>();
+            private int _id;
+
+            public DataBase()
+            {
+                _players.Add(1, new Player(
+                    name: "Папич",
+                    level: 1));
+
+                _players.Add(2, new Player(
+                    name: "Первый",
+                    level: 2));
+
+                _players.Add(3, new Player(
+                    name: "Второй",
+                    level: 33));
+
+                for (int i = 0; i < _players.Count; i++)
+                    _names.Add(_players[i + 1].Name);
+
+                _id = _players.Count;
+            }
+
+            public Dictionary<int, Player> ShowPlayer()
+            {
+                if (IsFilled() == false)
+                    MessageBox.Show("Она пуста!");
+
+                return _players;
+            }
+
+            public void AddPlayer(string name, string level)
+            {
+                if (name.Length > maxLengthName || name.Length < 1)
+                {
+                    MessageBox.Show("Ошибка ввода имени!");
+                    return;
+                }
+
+                if (SearchСopy(name))
+                {
+                    MessageBox.Show("Повторяющееся имя!");
+                    return;
+                }
+
+                if (!Int32.TryParse(level, out int levelInNumber))
+                {
+                    MessageBox.Show("Тут должны быть цифры!");
+                    return;
+                }
+
+                if (levelInNumber > maxLengthLevel || levelInNumber < 1)
+                {
+                    MessageBox.Show("Ошибка ввода уровня!");
+                    return;
+                }
+
+                _id++;
+                _names.Add(name);
+                _players.Add(_id, new Player(name, levelInNumber));
+
+                MessageBox.Show("Новый игрок успешно добавлен!");
+            }
+
+            public void BanPlayer(int id)
+            {
+                _players[id].SwitchBanStatus(true);
+            }
+
+            public void UnbanPlayer(int id)
+            {
+                _players[id].SwitchBanStatus(false);
+            }
+
+            public void DeletePlayer(int id)
+            {
+                _names.Remove(_players[id].Name);
+
+                if (_players.Remove(id))
+                {
+                    MessageBox.Show($"\nУдаление прошло успешно!");
+                    return;
+                }
+            }
+
+            public bool SearchСopy(string name)
+            {
+                if (_names.Contains(name))
+                    return true;
+
+                return false;
+            }
+
+            private bool IsFilled()
+            {
+                if (_players.Count > 0)
+                    return true;
+
+                MessageBox.Show("\nБаза данных пуста!");
+                return false;
+            }
+
+            public string GetBanStatus(int id) => _players[id].GetBanStatus();
+            public string GetName(int id) => _players[id].Name;
+            public int GetLevel(int id) => _players[id].Level;
+        }
+
+        class Player
+        {
+            private bool _isBanned;
+
+            public Player(string name, int level)
+            {
+                Name = name;
+                Level = level;
+                _isBanned = false;
+            }
+
+            public string Name { get; private set; }
+            public int Level { get; private set; }
+
+            public void SwitchBanStatus(bool isBanned)
+            {
+                if (IsStatusSame(isBanned))
+                    return;
+
+                _isBanned = isBanned;
+                string message;
+
+                if (_isBanned)
+                    message = $"\n{Name} забанен!";
+                else
+                    message = $"\n{Name} разбанен!";
+
+                MessageBox.Show(message);
+            }
+
+            public string GetBanStatus()
+            {
+                if (_isBanned)
+                    return "забанен";
+
+                return "не забанен";
+            }
+
+            private bool IsStatusSame(bool isBanned)
+            {
+                if (_isBanned != isBanned)
+                    return false;
+
+                if (_isBanned)
+                {
+                    MessageBox.Show($"\n{Name} и так забанен!");
+                    return true;
+                }
+
+                MessageBox.Show($"\n{Name} не забанен!");
+                return true;
+            }
+        }
+
+        DataBase dataBase = new DataBase();
+
+        public Form1()
+        {
+            InitializeComponent();
+            BackColor = Color.White;
+            textWriter();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataBase.AddPlayer(textBox1.Text, textBox2.Text);
+            textWriter();
+
+            if (dataBase.SearchСopy(textBox1.Text))
+            {
+                textBox1.BackColor = Color.Red;
+                return;
+            }
+            else
+                textBox1.BackColor = Color.White;
+        }
+
+        private bool TryGetId(out int id)
+        {
+            id = 0;
+
+            if (textBox3.Text == null)
+                return false;
+
+            if (!Int32.TryParse(textBox3.Text, out id))
+            {
+                MessageBox.Show("Некорректный ID!");
+                return false;
+            }
+
+            if (dataBase.ShowPlayer().ContainsKey(id))
+                return true;
+
+            MessageBox.Show("\nТакое ID не найдено!");
+
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (TryGetId(out int id))
+                dataBase.BanPlayer(id);
+
+            textWriter();
+            textBox3.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (TryGetId(out int id))
+                dataBase.UnbanPlayer(id);
+
+            textWriter();
+            textBox3.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (TryGetId(out int id))
+                dataBase.DeletePlayer(id);
+
+            textWriter();
+            textBox3.Clear();
+        }
+
+        private void textWriter()
+        {
+            Dictionary<int, Player> _p = dataBase.ShowPlayer();
+            listBox1.Text = "";
+            string spase = "";
+            foreach (KeyValuePair<int, Player> player in _p)
+            {
+                int personalId = player.Key;
+                listBox1.Text = listBox1.Text + $"|  Id: {personalId}  " +
+                                                $"|  Статус бана: {dataBase.GetBanStatus(personalId)}  " +
+                                                $"|  Уровень: {dataBase.GetLevel(personalId)}  " +
+                                                $"|  {dataBase.GetName(personalId)}  \n" ;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            textWriter();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+
+            if (name.Length > maxLengthName || name.Length < 1)
+            {
+                textBox1.BackColor = Color.Red;
+                return;
+            }
+            else
+                textBox1.BackColor = Color.White;
+
+            if (dataBase.SearchСopy(name))
+            {
+                textBox1.BackColor = Color.Red;
+                return;
+            }
+            else
+                textBox1.BackColor = Color.White;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case 82:
+                    rCode = true;
+                    eCode = false;
+                    dCode = false;
+                    break;
+
+                case 69:
+                    eCode = true;
+                    dCode = false;
+                    break;
+
+                case 68:
+                    dCode = true;
+                    break;
+
+                default:
+                    BackColor = Color.White;
+                    rCode = false;
+                    eCode = false;
+                    dCode = false;
+                    break;
+            }
+
+            if ((rCode, eCode, dCode) == (true, true, true))
+            {
+                BackColor = Color.Red;
+                rCode = false;
+                eCode = false;
+                dCode = false;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int level = 1;
+
+            if (textBox2.Text != "")
+                level = Convert.ToInt32(textBox2.Text);
+
+            if (level > maxLengthLevel || level <= 0)
+            {
+                textBox2.BackColor = Color.Red;
+                return;
+            }
+            else
+                textBox2.BackColor = Color.White;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == true)
+                return;
+
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+                return;
+
+            e.Handled = true;
+            textBox2.Clear();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == true)
+                return;
+
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+                return;
+
+            e.Handled = true;
+            textBox3.Clear();
+        }
+    }
+}
